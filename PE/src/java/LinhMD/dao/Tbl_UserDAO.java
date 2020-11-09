e234b3863971cd703f21dfd538a580867a377806
TWFpIMSQw6xuaCBMaW5o <@SE140063>
2020-11-09 07:53:58 +0700

    Commit at 637405052379457652 - 2861.5659521s

--- PE/src/java/LinhMD/dao/Tbl_UserDAO.java
+++ PE/src/java/LinhMD/dao/Tbl_UserDAO.java
@@ -5,10 +5,27 @@
  */
 package LinhMD.dao;
 
+import LinhMD.ultil.DBHelpers;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+
 /**
  *
  * @author USER
  */
 public class Tbl_UserDAO {
-    
+    public boolean checkLogin(String username, int password) throws ClassNotFoundException, SQLException, NamingException{
+        Connection c = null;
+        String statement = "select userId, password form tbl_User where userId=?";
+        PreparedStatement ps = null;
+        try{
+            c = DBHelpers.makeConnection();
+            c.prepareStatement(statement);
+            
+        } 
+        return false;
+    }
 }
