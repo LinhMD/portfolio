a40a30edea39f71918fe44ac99ade7b64f22ce3a
TWFpIMSQw6xuaCBMaW5o <@SE140063>
2020-11-09 07:47:37 +0700

    Commit at 637405048579401791 - 2481.560366s

--- PE/src/java/LinhMD/Servlet/LoginServlet.java
+++ PE/src/java/LinhMD/Servlet/LoginServlet.java
@@ -30,17 +30,13 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+        String username = request.getParameter("txtUsername");
+        String password = request.getParameter("txtPassword");
+        try {
+            
+        }finally{
+            out.close();
         }
     }
 
