b49edb6f3dc19d143465ae6e156b3795f6636b53
TWFpIMSQw6xuaCBMaW5o <@SE140063>
2020-11-09 08:24:58 +0700

    Commit at 637405070979396055 - 4721.5597924s

--- PE/src/java/LinhMD/Servlet/ShowClubServlet.java
+++ PE/src/java/LinhMD/Servlet/ShowClubServlet.java
@@ -30,17 +30,11 @@ public class ShowClubServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ShowClubServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ShowClubServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+        try {
+            
+        }finally{
+            out.close();
         }
     }
 
