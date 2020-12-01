fb915b8fb562e2f230988d69a995b566a7213f02
LinhMD <maidinhlinh967@gmail.com>
2020-12-01 19:46:56 +0700

    init

--- not-that-importance-delete-later/src/LinhMD/MathUtility.java
+++ not-that-importance-delete-later/src/LinhMD/MathUtility.java
@@ -12,13 +12,19 @@ package LinhMD;
 public class MathUtility {
     public static final double PI = 3.14159;
     public static long getFactorial(int n){
-        if(n < 0 || n > 20) throw new IllegalArgumentException("Input to big");
+        if(n < 0 || n > 20) throw new IllegalArgumentException("n => 0 && n <= 20");
         if(n == 0)
             return 1;
-        else 
-            return n * getFactorial(n - 1);
+
+        return n * getFactorial(n - 1);
     }
+
     public static void main(String[] args) {
-        System.out.println(getFactorial(4));
+        for (int i = 0; i < 20; i++) {
+            System.out.println(getFactorial(i));
+        }
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(getFactorial(-5));
+        System.out.println("nah");
     }
 }
