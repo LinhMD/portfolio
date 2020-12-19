d2db0359d23d5bfd4ceb6a772042657101e91173
LinhMD <maidinhlinh967@gmail.com>
2020-12-20 03:07:15 +0700

    init

---
+++ test/src/main/java/com/example/test/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.test.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String hello(){
+		return "hello world";
+	}
+}
